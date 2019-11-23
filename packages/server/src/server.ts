4ea53307bcbc947a339b6df62458b9d2c96bcbae
Cabe <sebastian.scd@gmail.com>
2019-11-24 11:38:53 +1300

    add connection to mongoose atlas, and route to products

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,12 +1,30 @@
 import * as express from "express";
+import { connect } from "mongoose";
+import { allProducts } from "./controllers/productController";
 
 const app = express();
 const port = 8899;
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send("Hello world 2!");
 });
+app.get("/products", allProducts);
 
 app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
+  console.log(`Server started at http://localhost:${port}`);
 });
+
+const uri: string =
+  "mongodb+srv://cabezonidas:TestPassword1407@repocluster-exdit.mongodb.net/test?retryWrites=true&w=majority";
+
+connect(
+  uri,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err: any) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log("Successfully Connected!");
+    }
+  }
+);
