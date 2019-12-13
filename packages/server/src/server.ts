21c8402e88dc902560d1efe0019bd0d673886d63
Cabe <sebastian.scd@gmail.com>
2019-12-14 10:58:20 +1300

    Add product routes

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,14 +1,24 @@
 import * as express from "express";
 import { connect } from "mongoose";
-import { allProducts } from "./controllers/productController";
+import {
+  allProducts,
+  addProduct,
+  deleteProduct,
+  getProduct,
+} from "./controllers/productController";
 
 const app = express();
 const port = 8899;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello world 2!");
 });
 app.get("/products", allProducts);
+app.post("/products/add", addProduct);
+app.delete("/products/delete", deleteProduct);
+app.get("/products/:id", getProduct);
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
