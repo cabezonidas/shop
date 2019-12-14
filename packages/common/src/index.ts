16dc3ef85a68e9d4a5ded3014690e3b64c143fcc
Cabe <sebastian.scd@gmail.com>
2019-12-14 19:57:35 +1300

    Add index file to common package with secretSanta function

---
+++ packages/common/src/index.ts
@@ -0,0 +1,39 @@
+export const getDigits = () => {
+  return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+};
+
+interface IParticipant {
+  name: string;
+  phone: string;
+}
+
+export const secretSanta = (participants: IParticipant[]) => {
+  const indexedParticipants = participants.map((p, i) => ({ ...p, id: i }));
+
+  const selectedDestinations: number[] = [];
+  const result: Array<{ from: IParticipant; to: IParticipant }> = [];
+
+  const getUnselectedParticipants = (
+    participants: IParticipant[],
+    currentNumber: number,
+    selectedDestination: number[]
+  ): number => {
+    const newDest = Math.floor(Math.random() * participants.length);
+    if (selectedDestination.includes(newDest) || currentNumber === newDest) {
+      return getUnselectedParticipants(participants, currentNumber, selectedDestination);
+    } else {
+      return newDest;
+    }
+  };
+
+  indexedParticipants.forEach(p => {
+    const to = getUnselectedParticipants(participants, p.id, selectedDestinations);
+    selectedDestinations.push(to);
+    result.push({
+      from: p,
+      to: participants[to],
+    });
+  });
+
+  return result;
+};
