7440b0338c494b2baecf758c5351efd4c5d28787
Cabe <sebastian.scd@gmail.com>
2019-12-14 20:10:35 +1300

    Implement secret santa!

--- packages/front-end/src/App.tsx
+++ packages/front-end/src/App.tsx
@@ -1,23 +1,46 @@
 import React from "react";
 import "./App.css";
 import { Square } from "@cabezonidas/shop-ui";
+import { secretSanta } from "@cabezonidas/shop-common";
+
+const myParticipants = [
+  { name: "nico", phone: "123456" },
+  { name: "lelu", phone: "123456" },
+  { name: "fiore", phone: "123456" },
+  { name: "ali", phone: "123456" },
+  { name: "tia", phone: "123456" },
+  { name: "ma", phone: "123456" },
+  { name: "dani", phone: "123456" },
+  { name: "pa", phone: "123456" },
+  { name: "seba", phone: "123456" },
+  { name: "cu", phone: "123456" },
+];
 
 const App: React.FC = () => {
+  const gifts = secretSanta(myParticipants);
   return (
     <div className="App">
       <header className="App-header">
+        <table>
+          <thead>
+            <tr>
+              <th>From</th>
+              <th>To</th>
+            </tr>
+          </thead>
+          <tbody>
+            {gifts.map((g, i) => (
+              <tr key={i}>
+                <td>
+                  {g.from.phone} ({g.from.name})
+                </td>
+                <td>{g.to.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
         <Square />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
