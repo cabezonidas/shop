e057f8a301c7aad6aee56c7ae40345b862f0350a
Cabe <sebastian.scd@gmail.com>
2019-12-16 09:09:24 +1300

    Update Secret Santa function

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -5,35 +5,54 @@ export const getDigits = () => {
 interface IParticipant {
   name: string;
   phone: string;
+  overseas: boolean;
+  sendsOverseas: boolean;
 }
 
-export const secretSanta = (participants: IParticipant[]) => {
-  const indexedParticipants = participants.map((p, i) => ({ ...p, id: i }));
+export const secretSanta = (
+  participants: IParticipant[]
+): {
+  from: IParticipant;
+  to: IParticipant;
+}[] => {
+  try {
+    const indexedParticipants = participants.map((p, i) => ({ ...p, id: i }));
 
-  const selectedDestinations: number[] = [];
-  const result: Array<{ from: IParticipant; to: IParticipant }> = [];
+    const selectedDestinations: number[] = [];
+    const result: Array<{ from: IParticipant; to: IParticipant }> = [];
 
-  const getUnselectedParticipants = (
-    participants: IParticipant[],
-    currentNumber: number,
-    selectedDestination: number[]
-  ): number => {
-    const newDest = Math.floor(Math.random() * participants.length);
-    if (selectedDestination.includes(newDest) || currentNumber === newDest) {
-      return getUnselectedParticipants(participants, currentNumber, selectedDestination);
-    } else {
-      return newDest;
-    }
-  };
+    const getUnselectedParticipants = (
+      participants: IParticipant[],
+      currentNumber: number,
+      selectedDestination: number[]
+    ): number => {
+      const newDest = Math.floor(Math.random() * participants.length);
+      if (selectedDestination.includes(newDest) || currentNumber === newDest) {
+        return getUnselectedParticipants(participants, currentNumber, selectedDestination);
+      } else {
+        return newDest;
+      }
+    };
 
-  indexedParticipants.forEach(({ id, ...p }) => {
-    const to = getUnselectedParticipants(participants, id, selectedDestinations);
-    selectedDestinations.push(to);
-    result.push({
-      from: p,
-      to: participants[to],
+    indexedParticipants.forEach(({ id, ...p }) => {
+      const to = getUnselectedParticipants(participants, id, selectedDestinations);
+      selectedDestinations.push(to);
+      result.push({
+        from: p,
+        to: participants[to],
+      });
     });
-  });
 
-  return result;
+    const conflicting = result.filter(
+      r => (!r.from.sendsOverseas && r.to.overseas) || (r.from.overseas && !r.to.sendsOverseas)
+    );
+
+    if (conflicting.length) {
+      return secretSanta(participants);
+    }
+
+    return result;
+  } catch {
+    return secretSanta(participants);
+  }
 };
