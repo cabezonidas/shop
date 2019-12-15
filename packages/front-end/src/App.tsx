61b84d2e134c108b36a16f8760b2b8c3e5206e71
Cabe <sebastian.scd@gmail.com>
2019-12-16 09:09:51 +1300

    Use Show Secret Santa in main app

--- packages/front-end/src/App.tsx
+++ packages/front-end/src/App.tsx
@@ -1,45 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Square } from "@cabezonidas/shop-ui";
 import { secretSanta } from "@cabezonidas/shop-common";
 
 const myParticipants = [
-  { name: "nico", phone: "123456" },
-  { name: "lelu", phone: "123456" },
-  { name: "fiore", phone: "123456" },
-  { name: "ali", phone: "123456" },
-  { name: "tia", phone: "123456" },
-  { name: "ma", phone: "123456" },
-  { name: "dani", phone: "123456" },
-  { name: "pa", phone: "123456" },
-  { name: "seba", phone: "123456" },
-  { name: "cu", phone: "123456" },
+  { name: "Nico cuñado", phone: "91122447624", overseas: false, sendsOverseas: false },
+  { name: "Fiore", phone: "91162971368", overseas: false, sendsOverseas: false },
+  { name: "Seba", phone: "+642102790126", overseas: true, sendsOverseas: true },
+  { name: "Lucia", phone: "+6421780906", overseas: true, sendsOverseas: true },
+  { name: "Ali", phone: "91161895704", overseas: false, sendsOverseas: true },
+  { name: "Claudio", phone: "91169176429", overseas: false, sendsOverseas: true },
+  { name: "Gabiruli", phone: "91168207507", overseas: false, sendsOverseas: true },
+  { name: "Lelu", phone: "91128873280", overseas: false, sendsOverseas: true },
+  { name: "Marce", phone: "91128261139", overseas: false, sendsOverseas: true },
+  { name: "Nico primo", phone: "91130783205", overseas: false, sendsOverseas: true },
+  { name: "Vicky", phone: "91138480501", overseas: false, sendsOverseas: true },
 ];
-
+const gifts = secretSanta(myParticipants.sort(() => Math.random() - 0.5));
 const App: React.FC = () => {
-  const gifts = secretSanta(myParticipants);
+  const argentinianDesignations = gifts.filter(g => !g.from.overseas);
+  const luciaDesignation = gifts.find(g => g.from.name === "Lucia");
+  const sebaDesignation = gifts.find(g => g.from.name === "Seba");
+
+  const [showArgs, setShowArgs] = useState(false);
+  const [showSeb, setShowSeb] = useState(false);
+  const [showLucia, setShowLucia] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <table>
-          <thead>
-            <tr>
-              <th>From</th>
-              <th>To</th>
-            </tr>
-          </thead>
-          <tbody>
-            {gifts.map((g, i) => (
-              <tr key={i}>
-                <td>
-                  {g.from.phone} ({g.from.name})
-                </td>
-                <td>{g.to.name}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-
+        <div style={{ display: "flex" }}>
+          <div style={{ margin: 20, padding: 20, border: "1px solid white" }}>
+            <div style={{ borderBottom: "1px solid white" }}>Participantes Argentila</div>
+            <input type="checkbox" onChange={() => setShowArgs(s => !s)} checked={showArgs} />
+            {showArgs && (
+              <table>
+                <thead>
+                  <tr>
+                    <th>From</th>
+                    <th>To</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {argentinianDesignations.map((g, i) => (
+                    <tr key={i}>
+                      <td>{g.from.phone}</td>
+                      <td>{g.to.name}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+          </div>
+          <div style={{ margin: 20, padding: 20, border: "1px solid white" }}>
+            <div style={{ borderBottom: "1px solid white" }}>Lu</div>
+            <input type="checkbox" onChange={() => setShowLucia(s => !s)} checked={showLucia} />
+            {showLucia && luciaDesignation && <small>{luciaDesignation.to.name}</small>}
+          </div>
+          <div style={{ margin: 20, padding: 20, border: "1px solid white" }}>
+            <div style={{ borderBottom: "1px solid white" }}>Seba</div>
+            <input type="checkbox" onChange={() => setShowSeb(s => !s)} checked={showSeb} />
+            {showSeb && sebaDesignation && <small>{sebaDesignation.to.name}</small>}
+          </div>
+        </div>
         <Square />
       </header>
     </div>
