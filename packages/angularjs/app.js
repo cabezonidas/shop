cfc29be0538a34b0585a8f11aa16b7615d59da33
Cabe <sebastian.scd@gmail.com>
2019-12-21 14:38:56 +1300

    Render react app & react component inside angularjs

--- packages/angularjs/app.js
+++ packages/angularjs/app.js
@@ -1,6 +1,10 @@
 const { Square } = require("@cabezonidas/shop-ui");
+const { App } = require("@cabezonidas/shop-sub-app");
 const { react2angular } = require("react2angular");
 
-angular.module("app", ["components"]);
+angular.module("app", ["react"]);
 
-angular.module("components", []).component("myComponent", react2angular(Square, []));
+angular
+  .module("react", [])
+  .component("myComponent", react2angular(Square, []))
+  .component("myApp", react2angular(App, []));
