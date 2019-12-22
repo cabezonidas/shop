310715225b94ea57805abd357496f274fdaf9aba
Cabe <sebastian.scd@gmail.com>
2019-12-23 11:22:29 +1300

    Put server code inside async function

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -2,23 +2,23 @@ import "dotenv/config";
 import "reflect-metadata";
 import * as express from "express";
 import { ApolloServer } from "apollo-server-express";
-import typeDefs from "./type-defs";
-import resolvers from "./resolvers";
 import { createConnection } from "typeorm";
 import { mongodbConnection } from "../ormconfig";
 import * as cookieParser from "cookie-parser";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./resolvers/user-resolver";
 
-createConnection(mongodbConnection)
-  .then(() => console.log("Connected to DB"))
-  .catch(error => console.log("Error: ", error));
+(async () => {
+  await createConnection(mongodbConnection);
 
-const port = process.env.PORT;
-const app = express();
-app.get("/", (_, res) => res.send("Home route"));
-const server = new ApolloServer({ typeDefs, resolvers });
-app.use(cookieParser());
-server.applyMiddleware({ app });
+  const port = process.env.PORT;
+  const app = express();
+  app.get("/", (_, res) => res.send("Home route"));
+  const server = new ApolloServer({ schema: await buildSchema({ resolvers: [UserResolver] }) });
+  app.use(cookieParser());
+  server.applyMiddleware({ app });
 
-app.listen({ port }, () =>
-  console.log(`Graphql server ready at http://localhost:${port}${server.graphqlPath}`)
-);
+  app.listen({ port }, () =>
+    console.log(`Graphql server ready at http://localhost:${port}${server.graphqlPath}`)
+  );
+})();
