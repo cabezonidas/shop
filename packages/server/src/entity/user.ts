1dc679ef327c843ae371e621e5e69ce644c21136
Cabe <sebastian.scd@gmail.com>
2019-12-23 13:25:01 +1300

    Add graphql-type

--- packages/server/src/entity/user.ts
+++ packages/server/src/entity/user.ts
@@ -1,10 +1,14 @@
 import { Entity, Column, BaseEntity, ObjectIdColumn, ObjectID } from "typeorm";
+import { ObjectType, Field } from "type-graphql";
 
+@ObjectType()
 @Entity("users")
 export class User extends BaseEntity {
+  @Field(() => String)
   @ObjectIdColumn()
   public id: ObjectID;
 
+  @Field()
   @Column()
   public email: string;
 
