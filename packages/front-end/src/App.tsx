e46400b5ad53f74aa827d6254e3b95846a77b933
Cabe <sebastian.scd@gmail.com>
2019-12-24 11:09:38 +1300

    Show users in front-end

--- packages/front-end/src/App.tsx
+++ packages/front-end/src/App.tsx
@@ -3,12 +3,26 @@ import "./App.css";
 import { Square } from "@cabezonidas/shop-ui";
 import { App as SubApp } from "@cabezonidas/shop-sub-app";
 import { SecretSanta } from "./secret-santa";
+import { useUsersQuery } from "./generated/graphql";
 
 const App: React.FC = () => {
+  const { data, loading } = useUsersQuery();
+
+  if (loading || !data) {
+    return <div>loading...</div>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <SecretSanta />
+        {data && (
+          <div>
+            {data.users.map(u => (
+              <div key={u._id}>{u.email}</div>
+            ))}
+          </div>
+        )}
         <Square />
         <SubApp />
       </header>
