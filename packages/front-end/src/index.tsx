997347bedece722031ef2c0aa4f9ae1eb5109f29
Cabe <sebastian.scd@gmail.com>
2019-12-24 14:04:44 +1300

    Add access token to header

--- packages/front-end/src/index.tsx
+++ packages/front-end/src/index.tsx
@@ -5,8 +5,20 @@ import App from "./App";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 import * as serviceWorker from "./serviceWorker";
+import { getAccessToken } from "./accessToken";
 
-const client = new ApolloClient({ uri: "http://localhost:8899/graphql", credentials: "include" });
+const client = new ApolloClient({
+  uri: "http://localhost:8899/graphql",
+  credentials: "include",
+  request: operation => {
+    const accessToken = getAccessToken();
+    if (accessToken) {
+      operation.setContext({
+        headers: { authorization: `bearer ${accessToken}` },
+      });
+    }
+  },
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
