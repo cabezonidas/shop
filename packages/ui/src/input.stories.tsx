b38f7970987ad78607a5ffcca263151afcf5060b
Cabe <sebastian.scd@gmail.com>
2019-12-27 15:17:27 +1300

    Add components to UI

---
+++ packages/ui/src/input.stories.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Input } from "./input";
+
+storiesOf("Input", module).add("Default", () => <Input width="200px" />);
