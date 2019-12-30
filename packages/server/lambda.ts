980e1c13f29e12755c802881e4b54866749ddfa7
Cabe <sebastian.scd@gmail.com>
2019-12-30 21:22:08 +1300

    Add cors to lambda

--- packages/server/lambda.ts
+++ packages/server/lambda.ts
@@ -2,6 +2,9 @@ import { createServer, proxy } from "aws-serverless-express";
 import app from "./src/server";
 import { connectToDatabase } from "./src/db";
 import { Context } from "aws-lambda";
+import * as middy from "middy";
+import { httpErrorHandler, cors } from "middy/middlewares";
+import { whiteList } from "./src/middleware";
 
 const binaryMimeTypes = [
   "application/octet-stream",
@@ -15,8 +18,10 @@ const binaryMimeTypes = [
 
 const server = createServer(app, null, binaryMimeTypes);
 
-export const backend = async (event: any, context: Context) => {
-  // context.callbackWaitsForEmptyEventLoop = false;
+export const backend = middy(async (event: any, context: Context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
   await connectToDatabase();
   return proxy(server, event, context, "PROMISE").promise;
-};
+})
+  .use(httpErrorHandler())
+  .use(cors({ origins: whiteList, credentials: true }));
