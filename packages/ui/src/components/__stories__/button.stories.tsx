ac86564d1f339c79d8ac4e8bb8cf9e5bf523c76a
Cabe <sebastian.scd@gmail.com>
2020-02-01 12:08:39 +1300

    Revert "Maybe fix deploy??"
    This reverts commit cba732c5b05d66f790e559215214e742cc628a92.

--- packages/ui/src/components/__stories__/button.stories.tsx
+++ packages/ui/src/components/__stories__/button.stories.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-// import { storiesOf } from "@storybook/react";
+import { storiesOf } from "@storybook/react";
 import { Button, PrimaryButton } from "../button";
 import { Box } from "../box";
 
-// storiesOf("Buttons", module)
-//   .add("Default", () => (
-//     <Button bg="green" width="250px">
-//       <Box bg="green">{String("Test")}</Box>
-//     </Button>
-//   ))
-//   .add("Primary", () => (
-//     <PrimaryButton width="250px">
-//       <Box bg="green">{String("Test")}</Box>
-//     </PrimaryButton>
-//   ));
+storiesOf("Buttons", module)
+  .add("Default", () => (
+    <Button bg="green" width="250px">
+      <Box bg="green">{String("Test")}</Box>
+    </Button>
+  ))
+  .add("Primary", () => (
+    <PrimaryButton width="250px">
+      <Box bg="green">{String("Test")}</Box>
+    </PrimaryButton>
+  ));
--- packages/ui/src/components/__stories__/input.stories.tsx
+++ packages/ui/src/components/__stories__/input.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { storiesOf } from "@storybook/react";
+import { storiesOf } from "@storybook/react";
 import { Input } from "..";
 
-// storiesOf("Input", module).add("Default", () => <Input width="200px" />);
+storiesOf("Input", module).add("Default", () => <Input width="200px" />);
