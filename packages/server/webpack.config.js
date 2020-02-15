6bb4f06acf2bf1f07c872db0402bc3b49415b786
Cabe <sebastian.scd@gmail.com>
2020-02-16 00:10:30 +1300

    Maybe fix build

--- packages/server/webpack.config.js
+++ packages/server/webpack.config.js
@@ -1,26 +1,23 @@
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
-const { NODE_ENV = "production" } = process.env;
 module.exports = {
   entry: "./lambda.ts",
   devtool: "inline-source-map",
-  mode: NODE_ENV,
-  target: "node",
-  output: {
-    path: path.resolve(__dirname, "lib"),
-    filename: "lambda.js",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
     extensions: [".ts", ".mjs", ".js"],
   },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: ["ts-loader"],
-        exclude: /node_modules/,
-      },
-    ],
+  output: {
+    filename: "lambda.js",
+    path: path.resolve(__dirname, "lib"),
   },
   externals: [nodeExternals()],
 };
