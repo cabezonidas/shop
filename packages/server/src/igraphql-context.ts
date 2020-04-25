936b2beaea6a3b44d9a4002ee7628c4a4b14a27b
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-04-26 08:17:34 +1200

    Fix types

--- packages/server/src/igraphql-context.ts
+++ packages/server/src/igraphql-context.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
+import { I18NextRequest } from "i18next-express-middleware";
 
 export interface IGraphqlContext {
-  req: Request;
+  req: Request & I18NextRequest;
   res: Response;
   payload?: { userId: string };
 }
